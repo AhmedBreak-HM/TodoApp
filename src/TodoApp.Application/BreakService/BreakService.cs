e64b85d89be29dde63125a896ee18bd66b65f793
ahmed break <ahmed_break8@yahoo.com>
2022-03-10 13:38:27 +0200

    Add project files.

---
+++ src/TodoApp.Application/BreakService/BreakService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace TodoApp.BreakService
+{
+    //[ExposeServices(typeof(IBreakService))]
+    public class BreakService : IBreakService
+    {
+        private readonly ILogger<BreakService> _logger;
+
+        public BreakService(ILogger<BreakService> logger)
+        {
+            _logger = logger;
+        }
+        public void DoSomething()
+        {
+            _logger.LogInformation("From Break Service");
+        }
+    }
+}
---
+++ src/TodoApp.HttpApi.Host/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TodoApp.BreakService;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace TodoApp.Controllers
+{
+    public class HomeController : AbpController
+    {
+        private readonly IBreakService _breakService;
+
+        public HomeController(IBreakService breakService)
+        {
+            _breakService = breakService;
+        }
+        public ActionResult Index()
+        {
+            _breakService.DoSomething();
+            return Redirect("~/swagger");
+        }
+    }
+}
